0179e40f92e4232f9d8e910b7715bda7fa10cd5f
Chris <christopher1995joyce@gmail.com>
2019-02-21 20:05:14 -0500

    minor changes

--- EmployeeTrackingSystem/Models/Employee.cs
+++ EmployeeTrackingSystem/Models/Employee.cs
@@ -12,23 +12,29 @@ namespace EmployeeTrackingSystem.Models
         [Key]
         public string Id { get; set; }
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int Zip { get; set; }
+        //[RegularExpression(@"^\+1 \(\d{3}\) \d{3}-\d{4}$", ErrorMessage = "Invalid Phone Number.")]
         public string Phone { get; set; }
         [DisplayName("Date Of Birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         [DisplayName("Hire Date")]
+        [DataType(DataType.Date)]
         public DateTime HireDate { get; set; }
         [DisplayName("License Number")]
         public int LicenseNumber { get; set; }
         [DisplayName("Certification Type")]
         public string CertificationType { get; set; }
         [DisplayName("Certification Start Date")]
+        [DataType(DataType.Date)]
         public DateTime CertificationStartDate { get; set; }
         [DisplayName("Certification End Date")]
+        [DataType(DataType.Date)]
         public DateTime CertificationEndDate { get; set; }
     }
 }
